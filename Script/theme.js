c4f422b0db2a7d2b302ea564357a3c037ac6dbcb
AlMahmud996 <siftiam@gmail.com>
2025-03-03 22:30:26 +0600

    All Done

--- Script/theme.js
+++ Script/theme.js
@@ -1,24 +1,13 @@
-// function getRandomColor() {
-//     const letters = '0123456789ABCDEF';
-//     let color = '#';
-//     for (let i = 0; i < 6; i++) {
-//         color += letters[Math.floor(Math.random() * 16)];
-//     }
-//     return color;
-// }
-
-function clickChangeBg() {
-
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-    let rgbValue = "#" + randomColor;
-    document.body.style.backgroundColor = rgbValue;
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
-document.getElementById('theme-btn').addEventListener("click", function() {
-    const bgColor = getRandomColor();
-    document.body.style.backgroundColor = bgColor;
-
-    document.getElementById('theme-btn').addEventListener("click", function() {
-        this.style.backgroundColor = "red";
-      });
-})
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomColor();
+}
+document.getElementById("theme-btn").addEventListener("click", changeBackgroundColor);
